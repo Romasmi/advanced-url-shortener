a3ca45abca12e965baa7b98a0d62f78810bfaf10
romasmi <rsmarsu@gmail.com>
2025-09-25 12:49:45 +0700

    Add database service.

---
+++ internal/database/database.go
@@ -0,0 +1,54 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"shorturl/internal/config"
+	"shorturl/internal/utils"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type DbConnection struct {
+	DB *pgxpool.Pool
+}
+
+func (c *DbConnection) Connect(config *config.Config) error {
+	pgConfig, err := pgxpool.ParseConfig(config.Database.URL)
+	if err != nil {
+		return fmt.Errorf("unable to parse database URL: %w", err)
+	}
+	pgConfig.MaxConns = int32(config.Database.MaxConnections)
+	pgConfig.MinConns = int32(config.Database.MinConnections)
+	pgConfig.MaxConnLifetime = utils.MinutesToNanoseconds(config.Database.MaxConnectionLifetime)
+	pgConfig.MaxConnIdleTime = utils.MinutesToNanoseconds(config.Database.MaxConnectionIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	c.DB, err = pgxpool.NewWithConfig(ctx, pgConfig)
+	if err != nil {
+		return fmt.Errorf("unable to connect to database: %w", err)
+	}
+
+	if err := c.Ping(); err != nil {
+		return fmt.Errorf("unable to ping database: %w", err)
+	}
+
+	return nil
+}
+
+func (c *DbConnection) Close() {
+	if c.DB != nil {
+		c.DB.Close()
+		log.Println("database connection closed")
+	}
+}
+
+func (c *DbConnection) Ping() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	return c.DB.Ping(ctx)
+}
