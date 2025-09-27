86ebfa64d5364c322a3f78b5abe64cfeeda3927a
romasmi <rsmarsu@gmail.com>
2025-09-27 23:46:40 +0700

    Add url model.

---
+++ internal/models/url.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type Url struct {
+	ID          string    `json:"id"`
+	OriginalUrl string    `json:"original_url"`
+	ShortUrl    string    `json:"short_url"`
+	CreatedAt   time.Time `json:"created_at"`
+}
